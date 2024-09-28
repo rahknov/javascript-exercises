05d21a9d75e01d1217e4cc5088a52bd53b37798f
rahknov <[email protected]>
2024-09-28 23:36:44 +0800

    Activity Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    newText=word.split('');
+    reverse ="";
+    for(let i= newText.length - 1; i>=0;i--){
+        reverse += newText[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
